8b38589329a1a495d005aa2205586ea8b76e0208
HongYang Qiu <qiuhongyang@askcoin.org>
2017-08-23 18:52:26 +0800

    添加注释

--- ASKSecp256k1/ASKSecp256k1/CKScep256k1/CKSecp256k1.h
+++ ASKSecp256k1/ASKSecp256k1/CKScep256k1/CKSecp256k1.h
@@ -8,8 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, CKSecp256k1VerifyError) {
+	<#MyEnumValueA#>,
+	<#MyEnumValueB#>,
+	<#MyEnumValueC#>,
+};
+
 @interface CKSecp256k1 : NSObject
 + (NSData *)generatePublicKeyWithPrivateKey:(NSData *)privateKeyData compression:(BOOL)isCompression;
 + (NSData *)compactSignData:(NSData *)msgData withPrivateKey:(NSData *)privateKeyData;
+
+/**
+ 用公钥验证签名
+
+ @param sigData 签名的数据
+ @param msgData 原数据
+ @param pubKeyData 公钥
+ @return 1 OK；0 不正确；-3 公钥读取失败；-4 签名读取失败
+ */
 + (NSInteger)verifySignedData:(NSData *)sigData withMessageData:(NSData *)msgData usePublickKey:(NSData *)pubKeyData;
 @end
